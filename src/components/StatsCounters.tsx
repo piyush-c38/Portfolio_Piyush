598c6e5a2c0207bd6f389b017115f93ccc2a821f
piyush-c38 <chandrakarpiyush36@gmail.com>
2025-06-19 09:34:42 +0530

    FEAT: updated the hero stats

--- src/components/StatsCounters.tsx
+++ src/components/StatsCounters.tsx
@@ -3,10 +3,10 @@ const StatsCounters = () => (
   <div className="flex flex-row md:flex-col gap-7 md:gap-10 mt-0 md:mt-6 w-full justify-center md:justify-start">
     <div>
       <div className="text-xl sm:text-2xl font-normal text-black/80 tracking-tight mb-1">
-        <span className="font-semibold text-[26px] sm:text-[32px] md:text-[36px]">+200</span>
+        <span className="font-semibold text-[26px] sm:text-[32px] md:text-[36px]">+10</span>
       </div>
       <div className="text-xs sm:text-base font-light text-black/50">
-        Projects Completed
+        Clients Served
       </div>
     </div>
     <div>
