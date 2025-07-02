a4f1c8d892e8f6a701114deb0b410a43790cfd81
piyush-c38 <chandrakarpiyush36@gmail.com>
2025-07-02 11:19:33 +0530

    UPDATE: Resume link

--- public/data/link.js
+++ public/data/link.js
@@ -1,5 +1,5 @@
 const Links = {
-    resumeLink: "https://drive.google.com/file/d/1hdIRK0R1IA5SWNT6mGVz5s8Di7IvOZFw/view?usp=sharing",
-}
+    resumeLink: "https://drive.google.com/file/d/1fQMzDGi6NZyiHhGGD-X0LX7WM90lueh0/view?usp=drive_link",
+};
 
 export default Links;
