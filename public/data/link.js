da452b9339787b9d5546b1c1886b06c1cb16acb5
Piyush Chandrakar <129875159+piyush-c38@users.noreply.github.com>
2025-07-05 10:57:31 +0530

    Updated resume link

--- public/data/link.js
+++ public/data/link.js
@@ -1,6 +1,6 @@
 const Links = {
   resumeLink:
-    "https://drive.google.com/file/d/1fQMzDGi6NZyiHhGGD-X0LX7WM90lueh0/view?usp=drive_link",
+    "https://drive.google.com/file/d/1nrArOxgTsFYjzzPtn-xXd2REjxmaYbeF/view?usp=sharing",
   personalEmail: "piyushch.ofc@gmail.com",
   studentEmail: "piyushc@iitbhilai.ac.in",
   linkedin: "https://www.linkedin.com/in/piyush-chandrakar",
