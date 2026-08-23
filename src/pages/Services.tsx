2c8d67fbdec515db9c7a807e415336adffbc3e0a
Piyush Chandrakar <chandrakarpiyush36@gmail.com>
2026-08-23 11:05:09 +0530

    Add Reveal to services section

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -48,9 +48,11 @@ const Services = () => {
 
         <section className="py-20">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="text-l sm:text-xl text-gray-600 max-w-3xl mx-auto text-center my-20">
-              {servicesContent.hero_description}
-            </div>
+            <Reveal>
+              <div className="text-l sm:text-xl text-gray-600 max-w-3xl mx-auto text-center my-20">
+                {servicesContent.hero_description}
+              </div>
+            </Reveal>
             <RevealGroup className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 justify-items-center" stagger={0.4}>
               {servicesContent.services.map((service) => (
                 <Reveal key={service.slug}>
